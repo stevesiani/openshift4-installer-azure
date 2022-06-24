f1a6215a4a4f0dc071e96b8e42c21e79bac04e77
Steve Siani <steve.alphonse.siani@gmail.com>
2022-06-24 18:55:07 -0400

    Openshift POC

--- ignition/versions.tf
+++ ignition/versions.tf
@@ -2,12 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
+      version = "=2.74.0"
     }
     ignition = {
       source = "community-terraform-providers/ignition"
     }
     local = {
       source = "hashicorp/local"
+      version = "=2.1.0"
     }
     null = {
       source = "hashicorp/null"
--- versions.tf
+++ versions.tf
@@ -4,15 +4,19 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
+      version = "=2.74.0"
     }
     local = {
       source = "hashicorp/local"
+      version = "=2.1.0"
     }
     random = {
       source = "hashicorp/random"
+      version = "=3.1.0"
     }
     tls = {
       source = "hashicorp/tls"
+      version = "=3.1.0"
     }
     template = {
       source = "hashicorp/template"
